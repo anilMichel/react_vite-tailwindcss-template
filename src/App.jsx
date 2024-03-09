ffd8076e4f94fe8ad97b18c95bcc529b557eaaad
anilMichel <anilkumarvoonna123456@gmail.com>
2024-03-09 17:48:31 +0530

    react_vite-tailwindcss-template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2 className='text-3xl text-center text-blue-500 display_text'>
+        This the template for react + vite + tailwindcss template
+      </h2>
+    </>
+  )
+}
+
+export default App
